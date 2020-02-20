73ab0e8a5ea9b5e39a525ceaf25542e6b9f6c49e
Michael Kennedy <mikeckennedy@gmail.com>
2020-02-19 21:06:31 -0800

    Lower case names only

--- markdown_subtemplate/__init__.py
+++ markdown_subtemplate/__init__.py
@@ -3,7 +3,7 @@ markdown_subtemplate - A template engine to render
 Markdown with external template imports and variable replacements.
 """
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __author__ = 'Michael Kennedy <michael@talkpython.fm>'
 __all__ = []
 
