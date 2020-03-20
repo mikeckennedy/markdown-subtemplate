bd32ee86d8096671cedb344c8ad52761d09495d4
Michael Kennedy <mikeckennedy@gmail.com>
2020-03-20 11:49:28 -0700

    Final version for releasing to pypi.org

--- setup.py
+++ setup.py
@@ -54,11 +54,12 @@ setup(
     install_requires=requires,
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
